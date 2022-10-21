9ef79b1538368b8b5ffc21b438ca63da87d926aa
GeoffNixCodes <97037675+GeoffNixCodes@users.noreply.github.com>
2022-10-21 16:18:25 -0400

    altered files, added state, new app

---
+++ src/props/Child.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+interface ChildProps {
+    color: string;
+    onClick: () => void;
+    children?: React.ReactNode;
+}
+
+export const Child = ({color, onClick}: ChildProps) => {
+    return <div>
+        {color}
+        <button onClick={onClick}> Click me</button>
+    </div>
+};
+
+export const ChildAsFC: React.FC<ChildProps> = ({color, onClick}) => {
+    return <div>{color}
+        <button onClick={onClick}> Click me</button>
+    </div>;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-expressions
+ChildAsFC.displayName
