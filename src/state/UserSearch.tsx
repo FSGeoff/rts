ea1e0d6f3552d807f35ffa07e2a6d42fe044f6e3
GeoffNixCodes <97037675+GeoffNixCodes@users.noreply.github.com>
2022-10-21 20:08:32 -0400

    added input element

---
+++ src/state/UserSearch.tsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+
+const users = [
+    {name: 'Kiara', age: 30},
+    {name: 'Inity', age: 16},
+    {name: 'Gyasi', age: 22}
+];
+
+const UserSearch: React.FC = () => {
+
+    const [name, setName] = useState('');
+    const [user, setUser] = useState<{name: string, age: number | undefined}>();
+    const onClick = () => {
+        setName('');
+        const foundUser = users.find((user) => {
+            return user.name === name;
+
+        });
+       setUser(foundUser);
+    };
+    return <div>
+       <h3>
+           User Search
+           <input value={name} onChange={e => setName(e.target.value)}/>
+           <button onClick={onClick}>Find User</button>
+           <div><br/>
+               {user && user.name}  {user && user.age}
+           </div>
+
+
+       </h3>
+
+    </div>
+};
+
+export default UserSearch;
