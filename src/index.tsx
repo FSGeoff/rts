3788260474adec8154f3c38bc728ff794165ea9f
GeoffNixCodes <97037675+GeoffNixCodes@users.noreply.github.com>
2022-10-22 21:40:12 -0400

    added useRef

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import GuestList  from "./state/GuestList";
-import UserSearch from "./state/UserSearch";
+
 import EventComponent from "./events/EventComponent";
+import UserSearch from './refs/UserSearch';
 
 const App = () => {
     return (
         <div>
-           <EventComponent />
+           <UserSearch />
         </div>
     )
 };
